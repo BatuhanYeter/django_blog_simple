e0847eb0bd1390883e7a1634ea52102caaf7f614
perseus325 <45850467+perseus325@users.noreply.github.com>
2020-08-21 14:47:32 +0300

    debuf off

--- Django_project/settings.py
+++ Django_project/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True # False yapmalıyız ki sitenin açığını bulmasınlar
+DEBUG = False # False yapmalıyız ki sitenin açığını bulmasınlar
 
 ALLOWED_HOSTS = ['mydjangoappbatu.herokuapp.com']
 
